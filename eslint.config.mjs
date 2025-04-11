858035802c7ef1f80d9b2cb7be5dbb1c2770d207
lgoubin <lou.goubin@gmail.com>
2025-04-11 10:29:37 +0200

    chore: update devDependencies to include ESLint packages for improved linting support

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+import json from "@eslint/json";
+import css from "@eslint/css";
+
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
